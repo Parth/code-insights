17792c47b0920268e9cd6cf5095d7cf6a9d3d623
parth <parth.mehrotra.cs@gmail.com>
2017-08-01 12:18:51 -0400

    psuedocode

--- backend/src/main/java/com/sap/codeinsights/API.java
+++ backend/src/main/java/com/sap/codeinsights/API.java
@@ -10,14 +10,4 @@ public class API {
 	public static String processorsAvailable() {
 		return ProcessorService.allProcessors();
 	}
-
-	//public static String getAllRecipes(MongoDatabase database) {
-	//	return RecipeService.getAllRecipes(database).toString();
-	//}
-
-	//public static String submitRecipe(MongoDatabase database, String json, StatusCode code) {
-	//	Gson gson = new Gson();
-	//	Recipe recipe = gson.fromJson(json, Recipe.class);
-	//	return RecipeService.submitRecipe(recipe, database).toString();
-	//}
 }
---
+++ backend/src/main/java/com/sap/codeinsights/ProcessorService.java
@@ -0,0 +1,23 @@
+package com.sap.codeinsights;
+
+public class ProcessorService {
+	public HashMap<String, String> jobs;
+
+	public static String allProcessors {
+		return "[Documentation]";
+	}
+
+	public static String createJob(Request req) {
+		createNewJob;
+		return jobID;
+	}
+
+	public static String jobStatus(String jobID) {
+		check job status;
+		return job status;
+	}
+
+	public static String getResult(String jobId) {
+		
+	}
+}
